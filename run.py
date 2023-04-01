4a356b68b62075fe7f8b871cf629985bda9e1c81
khaelk <klkable13@gmail.com>
2023-04-01 19:31:13 -0300

    add config reading

--- run.py
+++ run.py
@@ -1,5 +1,19 @@
 from simulation import Simulation
+from array import array
+import configparser
 
-sim = Simulation(2, 100000, 348, 'G', 'G', 1, 3, [1,2], [3,6])
+config = configparser.ConfigParser()
+config.read("config.ini")
+initialTime = int(config.get("configfile", "initialTime"))
+quantityNums = int(config.get("configfile", "quantityNums"))
+seed = int(config.get("configfile", "seed"))
+arrivals = config.get("configfile", "arrivals")
+service = config.get("configfile", "service")
+servers = int(config.get("configfile", "servers"))
+capacity = int(config.get("configfile", "capacity"))
+arrivalTime = eval(config.get("configfile", "arrivalTime"))
+serviceTime = eval(config.get("configfile", "serviceTime"))
+
+sim = Simulation(initialTime, quantityNums, seed, arrivals, service, servers, capacity, arrivalTime, serviceTime)
 sim.execute()
 print("perdas: ",sim.losses)
