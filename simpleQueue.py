dea6c1f31f0ba7ad5663b7c4afaa84e9a2485836
pablo.alles <p.chong@edu.pucrs.br>
2023-06-08 05:21:47 -0300

    big changes to queue network

--- simpleQueue.py
+++ simpleQueue.py
@@ -1,9 +1,11 @@
 class SimpleQueue:
-    def __init__(self, index, servers, capacity, arrivalTime, serviceTime):
-        self.index = index
+    def __init__(self, name, servers, capacity, arrivalTime, serviceTime, network):
+        self.name = name
         self.servers = servers
         self.capacity = capacity
         self.clients = 0 #num slots ocupados
         self.arrivalTime = arrivalTime
         self.serviceTime = serviceTime
         self.timeAtService = [0] * (capacity+1)
+        self.losses = 0
+        self.network = network
--- simulation.py
+++ simulation.py
@@ -2,12 +2,13 @@ from randomGen import GeneratedNums
 from simpleQueue import SimpleQueue
 
 class Simulation:
-    def __init__(self, initialTime, quantityNums, seed, simpleQueue1, simpleQueue2):
+    def __init__(self, initialTime, quantityNums, seed, queuesList, simpleQueue1, simpleQueue2):
         self.time = 0
         self.initialEventTime = initialTime
         self.nums = GeneratedNums(quantityNums, seed)
         self.usedNums = 0
         self.quantityNums = quantityNums
+        self.queuesList = queuesList
         self.simQueue1 = simpleQueue1
         self.simQueue2 = simpleQueue2
         self.losses = 0
@@ -21,71 +22,102 @@ class Simulation:
         return result
 
     #funcao que escalona eventos/realiza sorteio
-    def putSchedule(self, event):
+    def putSchedule(self, event, q1, q2):
         if self.usedNums>=self.quantityNums:
             return
         #sorteio
         if event == 'ch1':
-            eventTime = round(self.convert(self.simQueue1.arrivalTime[0],self.simQueue1.arrivalTime[1]) + self.time,4)
-            schedule = {'event': event, 'time': eventTime, 'queue': self.simQueue1.index}
+            eventTime = round(self.convert(q1.arrivalTime[0],q1.arrivalTime[1]) + self.time,4)
+            schedule = {'event': event, 'time': eventTime, 'queue': q1}
         if event == 'p12':
-            eventTime = round(self.convert(self.simQueue1.serviceTime[0], self.simQueue1.serviceTime[1]) + self.time,4)
-            schedule = {'event': event, 'time': eventTime, 'queue': self.simQueue1.index}
+            eventTime = round(self.convert(q1.serviceTime[0], q1.serviceTime[1]) + self.time,4)
+            schedule = {'event': event, 'time': eventTime, 'queue1': q1, 'queue2': q2}
         if event == 'sa2':
-            eventTime = round(self.convert(self.simQueue2.serviceTime[0],self.simQueue2.serviceTime[1]) + self.time,4)
-            schedule = {'event': event, 'time': eventTime, 'queue': self.simQueue2.index}
+            eventTime = round(self.convert(q1.serviceTime[0],q1.serviceTime[1]) + self.time,4)
+            schedule = {'event': event, 'time': eventTime, 'queue': q1}
         self.scheduler.append(schedule)
         #ordena o escalonador
         self.scheduler = sorted(self.scheduler, key=lambda x: float(x['time']))
 
     #funcao de start da simulacao
     def execute(self):
-        #primeira chegada escalonador
-        firstSchedule = {'event': 'ch1', 'time': self.initialEventTime, 'queue': self.simQueue1.index}
-        self.scheduler.append(firstSchedule)
+        #primeiras chegadas escalonador
+        for queue in self.queuesList:
+            if queue.arrivalTime != -1:
+                firstArrival = round(self.convert(queue.arrivalTime[0],queue.arrivalTime[1]) + self.time,4)
+                firstSchedule = {'event': 'ch1', 'time': firstArrival, 'queue': queue}
+                self.scheduler.append(firstSchedule)
         while(self.usedNums<self.quantityNums):
             #pega evento com 'menor tempo'
             event = self.scheduler.pop(0)
             print(event, end='\n\n')
             #contabiliza tempo
             #para cada fila contabilizar o tempo
-            self.simQueue1.timeAtService[self.simQueue1.clients]=round(self.simQueue1.timeAtService[self.simQueue1.clients] + event.get('time')-self.time,4)
-            self.simQueue2.timeAtService[self.simQueue2.clients]=round(self.simQueue2.timeAtService[self.simQueue2.clients] + event.get('time')-self.time,4)
+            for queue in self.queuesList:
+                queue.timeAtService[queue.clients]=round(queue.timeAtService[queue.clients] + event.get('time')-self.time,4)
+                if len(queue.timeAtService)<=30:
+                    print("name:" + queue.name + str(queue.timeAtService), end='\n\n')
+                else:
+                    print("name:" + queue.name + str(queue.timeAtService[:30]) + '...', end='\n\n')
+
             self.time = event.get('time')
-            print("index:" + str(self.simQueue1.index) + str(self.simQueue1.timeAtService), end='\n\n')
-            print("index:" + str(self.simQueue2.index) + str(self.simQueue2.timeAtService), end='\n\n')
             #print(self.time)
             if event.get('event') == 'ch1':
-                self.ch1()
+                self.ch1(event.get('queue'))
             if event.get('event') == 'p12':
-                self.p12()
+                self.p12(event.get('queue1'), event.get('queue2'))
             if event.get('event') == 'sa2':
-                self.sa2()
+                self.sa2(event.get('queue'))
+
+    def checkDestination(self, q, prob):
+        for dest in q.network:
+            if prob <= dest[1]:
+                return dest[0]
+            prob -= dest[1]
+        return
 
     #funcao de chegada na fila simples
-    def ch1(self):
-        if self.simQueue1.clients<self.simQueue1.capacity:
-            self.simQueue1.clients+=1
-            if self.simQueue1.clients<=self.simQueue1.servers:
-                self.putSchedule('p12')#agenda tranferencia f1 para f2
+    def ch1(self, q):
+        if q.clients<q.capacity:
+            q.clients+=1
+            if q.clients<=q.servers:
+                dest = self.checkDestination(q, self.convert(0,1))
+                if dest == 's':
+                    self.putSchedule('sa2', q, -1)#agenda saida do sistema
+                else:
+                    q2 = ''
+                    for q0 in self.queuesList:
+                        if q0.name == dest:
+                            q2 = q0
+                    self.putSchedule('p12', q, q2)#agenda tranferencia f1 para f2
         else:
             self.losses+=1
-        self.putSchedule('ch1')#chegada
+            q.losses+=1
+        self.putSchedule('ch1', q, -1)#chegada
     
     #funcao de transferencia f1 para f2
-    def p12(self):
-        self.simQueue1.clients-=1
-        if self.simQueue1.clients>=self.simQueue1.servers:
-            self.putSchedule('p12')#transferencia f1 f2
-        if self.simQueue2.clients<self.simQueue2.capacity:
-            self.simQueue2.clients+=1
-            if self.simQueue2.clients<=self.simQueue2.servers:
-                self.putSchedule('sa2')#saida f2
+    def p12(self, q1 , q2):
+        q1.clients-=1
+        if q1.clients>=q1.servers:
+            dest = self.checkDestination(q1, self.convert(0,1))
+            if dest == 's':
+                self.putSchedule('sa2', q1, -1)#agenda saida do sistema
+            else:
+                q2 = ''
+                for q0 in self.queuesList:
+                    if q0.name == dest:
+                        q2 = q0
+                self.putSchedule('p12', q1, q2)#agenda tranferencia f1 para f2
+        if q2.clients<q2.capacity:
+            q2.clients+=1
+            if q2.clients<=q2.servers:
+                self.putSchedule('sa2', q2, -1)#saida f2
         else:
             self.losses+=1
+            q2.losses+=1
 
     #funcao de saida na fila simples
-    def sa2(self):
-        self.simQueue2.clients-=1
-        if self.simQueue2.clients>=self.simQueue2.servers:
-            self.putSchedule('sa2')#saida f2
+    def sa2(self, q):
+        q.clients-=1
+        if q.clients>=q.servers:
+            self.putSchedule('sa2', q, -1)#saida f2
